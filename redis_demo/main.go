c81ad2b473139a585b1621e2caeba2233cff1b4c
shr26207 <sunhaoran@offcn.com>
2022-10-20 15:21:33 +0800

    redis-demo

--- redis_demo/main.go
+++ redis_demo/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/go-redis/redis/v8"
 )
+
+const script_del = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end"
 
 func main() {
 	rdb := redis.NewClient(&redis.Options{
@@ -15,14 +16,20 @@ func main() {
 		DB:       0,  // use default DB
 	})
 	ctx := context.Background()
-	result, err := rdb.Set(ctx, "r1", "111111", 60*time.Second).Result()
+	/*result, err := rdb.Set(ctx, "r1", "111111", 60*time.Second).Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(result)
+	fmt.Println(result)*/
 	get, err := rdb.Get(ctx, "r1").Result()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(get)
+
+	eval, err := rdb.Eval(ctx, script_del, []string{"r1"}, "6").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(eval)
 }
