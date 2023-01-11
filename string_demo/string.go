941e442b72a777995cafd8aa33621bb94e84bc02
shr26207 <sunhaoran@offcn.com>
2023-01-11 10:15:43 +0800

    Ucfirst

--- string_demo/string.go
+++ string_demo/string.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode"
 	"unicode/utf8"
 )
 
 func main() {
-	str := "你好";
-	fmt.Printf("%v\n",len(str))
-	fmt.Printf("%v\n",utf8.RuneCountInString(str))
+	str := "你好"
+	fmt.Printf("%v\n", len(str))
+	fmt.Printf("%v\n", utf8.RuneCountInString(str))
+	fmt.Println(strings.ToTitle("hello world"))
+	fmt.Println(strings.ToUpper("hello world"))
+	fmt.Println(Ucfirst("hello world"))
+}
+func Ucfirst(str string) (uc string) {
+	for i, c := range str {
+		uc = string(unicode.ToUpper(c)) + str[i+1:]
+		return
+	}
+	return
 }
