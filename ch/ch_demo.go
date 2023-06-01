b423670eaef971019c79eb10daf0b425f8c88707
shr26207 <sunhaoran@offcn.com>
2023-06-01 16:23:08 +0800

    excel.go

--- ch/ch_demo.go
+++ ch/ch_demo.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
 
-func main() {
-
+func demo1() {
 	ch1 := make(chan int, 10)
 	ch2 := make(chan int, 10)
 
@@ -31,4 +34,41 @@ func main() {
 		}
 		fmt.Println(num, num2)
 	}
+}
+func demo2() {
+	err_chen := make(chan error, 10)
+	var err_num, success_num int
+	var wg, wg1 sync.WaitGroup
+	wg1.Add(1)
+	go func() {
+		defer wg1.Done()
+		for err := range err_chen {
+			if err != nil {
+				err_num++
+			} else {
+				success_num++
+			}
+		}
+	}()
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+			defer wg.Done()
+			if i%2 == 0 {
+				err_chen <- errors.New("error")
+			} else {
+				err_chen <- nil
+			}
+		}(i)
+	}
+	wg.Wait()
+	close(err_chen)
+	wg1.Wait()
+	fmt.Println(err_num, success_num)
+}
+func main() {
+
+	demo2()
 }
--- ctx_val/ctx_value.go
+++ ctx_val/ctx_value.go
@@ -13,14 +13,16 @@ func HandelRequest(ctx context.Context) {
 			fmt.Println("HandelRequest Done.")
 			return
 		default:
-			fmt.Println("HandelRequest running, parameter: ", ctx.Value("parameter"))
+			fmt.Println("HandelRequest running, parameter: ", ctx.Value(contextTxKey{code: "b"}))
 			time.Sleep(2 * time.Second)
 		}
 	}
 }
 
+type contextTxKey struct{ code string }
+
 func main() {
-	ctx := context.WithValue(context.Background(), "parameter", "1")
+	ctx := context.WithValue(context.Background(), contextTxKey{code: "a"}, "1")
 	go HandelRequest(ctx)
 
 	time.Sleep(10 * time.Second)
